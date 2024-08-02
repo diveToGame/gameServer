84c7d3fbc44b1fe51736a3479c62fb49c92b8d56
stripy <aequatio1026@gmail.com>
2024-08-03 01:08:24 +0900

    refactor: :fire: remove task scheduler

--- src/common/common.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { TaskSchedulerService } from "./common.task-scheduler.service";
-
-@Module({
-  providers: [TaskSchedulerService],
-  exports: [TaskSchedulerService],
-})
-export class CommonModule {}
